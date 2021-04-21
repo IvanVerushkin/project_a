0b406c881c51452aae69b1e59e20ad4da81d0d7a
IvanVerushkin <tenekrut30@gmail.com>
2021-04-21 00:28:20 -0300

    do first page

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+const header__btn = document.querySelector('.header__btn');
+const rightside_menu = document.querySelector('.rightside-menu');
+const rightside_menu_close = document.querySelector('.rightside-menu__close');
+
+
+header__btn.addEventListener('click', () => {
+    rightside_menu.classList.remove('rightside-menu--close');
+});
+
+rightside_menu_close.addEventListener('click', () => {
+    rightside_menu.classList.add('rightside-menu--close');
+});
+
+$('.top__slider').slick ({
+    dots: true,
+    arrows: false,
+});
